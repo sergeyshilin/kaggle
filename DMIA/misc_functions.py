13b44d9011c5415965994f6b52ab719fe72e86b1
Dmitry Borody <dmitriy.borodiy@gmail.com>
2015-10-24 21:01:55 +0300

    Updated misc_functions.py

--- DMIA/misc_functions.py
+++ DMIA/misc_functions.py
@@ -1,3 +1,24 @@
+def build_features(data):
+    data['Word'] = data['Word'].apply(lambda word: word.decode('utf-8'))
+    data['Length'] = data['Word'].apply(lambda word: len(word))
+
+    vowels = [ 'а',  'я', 'ё', 'у','е', 'о', 'э', 'ю', 'и', 'ы', 'Ё', 'У', 'Е', 'Ы','А', 'О', 'Э', 'Ю', 'И', 'Я' ]
+    vowels = [let.decode('utf-8') for let in vowels]
+    data['Vowels'] = data['Word'].apply(lambda word: len([letter for letter in word if letter in vowels]))
+    data['Consonants'] = data['Length'] - data['Vowels']
+
+    data['is_lower'] = data['Word'].apply(lambda word: int( word[0] >= 'а'.decode('utf-8')))
+
+    data['Double'] = data['Word'].apply(lambda word: countDoubles(word))
+    data['Caps'] = data['Word'].apply(lambda x: x.upper() == x)
+    data['Frac_vowels'] = data['Vowels'].apply(float) / data['Consonants']
+    for i, substr in enumerate(['сон', 'ы', 'э', 'щ', 'ъ', 'й', 'ф']):
+        s = substr.decode('utf-8')
+        data['has_{}'.format(i)] = data['Word'].apply(lambda x: x.find(s) >= 0)
+    for i, substr in enumerate(['.*ин.?.?$', '.*ов.?.?$']):
+        s = substr.decode('utf-8')
+        data['has_re_{}'.format(i)] = data['Word'].apply(lambda x: re.match(substr, x, re.UNICODE) is not None)
+
 def check_substring(data, substr):
     has_substr = data[data['Word'].str.find(substr) > 0]
     is_name = has_substr[has_substr['Label'] == 1]
